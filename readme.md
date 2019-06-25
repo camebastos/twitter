eb6a76a847c183feba8c1e1434aeb2646f144213
camebastos <camebastos@gmail.com>
2019-06-25 19:08:23 -0300

    twitter refatorado versão final

---
+++ readme.md
@@ -0,0 +1,35 @@
+﻿# Twitter
+
+Neste desafio, vamos replicar o feed de notícias do Twitter.
+
+
+### Versão 0.0.1
+
+1.  Crie um formulário que permita inserir um texto e um botão para "twittar".
+2.  Adicione um evento de  _click_  ao botão ou de  _submit_  no formulário.
+3.  No evento, obter o texto.
+4.  Adicione o texto ao HTML.
+
+### Versão 0.0.2
+
+1.  Não inserir texto vazio (desativar o botão "twittar").
+2.  Conte o número de caracteres de forma regressiva.
+
+### Versão 0.0.3
+
+1.  Se você passar os 140 caracteres, desative o botão.
+2.  Se você passar os 120 caracteres, mostre o contador com outra cor.
+3.  Se você passar os 130 caracteres, mostre o contador com outra cor.
+4.  Se você passar os 140 caracteres, mostre o contador em negativo.
+
+### Versão 0.0.4
+
+1.  Ao pressionar enter (`/n`) aumente a área de texto de acordo com o tamanho do texto.
+
+### Versão 0.0.5 (Hacker edition)
+
+1.  Se o número de caracteres digitados (sem dar um "_enter_") exceder o tamanho da área de texto por padrão, mais uma linha deverá ser adicionada para que a rolagem não apareça. (Se aplicável)
+
+### Versão 0.0.6 (Hacker edition)
+
+1.  Adicione a hora em que o tweet foi publicado no formato de 24 horas  `hh:mm`.
---
+++ script.js
@@ -0,0 +1,86 @@
+let send = document.getElementById('send');
+let btn = document.getElementById('btn-post');
+let box = document.getElementById('box-post');
+const count = document.getElementById('count-post');
+const charMax = 140;
+
+box.addEventListener('keyup', btnBlocked, changeCollor);
+
+function btnBlocked() {
+    if (box.value.length === 0 || box.value.length > 140 || box.value === ' ') {
+        send.disabled = true;
+    } else {
+        send.disabled = false;
+    }
+}
+
+send.addEventListener('click', twittar);
+
+function twittar() {
+    let newTweet = document.createElement('li');
+    console.log(newTweet);
+
+    let list = document.getElementById('list-post');
+    console.log('retorno list', list);
+
+    newTweet.innerHTML = box.value;
+    console.log('o que printa na tela', newTweet);
+
+    list.appendChild(newTweet);
+    console.log('isso aqui é pra add o nó')
+
+    list.insertBefore(newTweet, list.firstChild);
+    console.log('ordem post', list);
+    box.value = '';
+
+
+    let dateHour = newTweet;
+    let newDate = document.createElement('p');
+
+    dateHour.insertBefore(newDate, dateHour.firstChild);
+    newDate.innerHTML = tweetTime();
+
+    btnBlocked();
+    charCount();
+
+}
+
+box.addEventListener('input', charCount);
+
+function charCount() {
+    let charDig = parseInt(box.value.length);
+    //  console.log(charDig);
+    let charRest = charMax - charDig;
+    count.innerHTML = charRest;
+    changeCollor();
+    autoResize();
+}
+
+box.addEventListener('keyup', changeCollor);
+
+
+function changeCollor() {
+    if (box.value.length < 120) {
+        count.style.color = "black";
+    } else if (box.value.length >= 120 && box.value.length <= 129) {
+        count.style.color = "green";
+    } else if (box.value.length >= 130 && box.value.length <= 139) {
+        count.style.color = "yellow";
+    } else if (box.value.length >= 140) {
+        count.style.color = "red";
+    }
+}
+
+function tweetTime() {
+    let date = new Date;
+    let hour = date.getHours().toString();
+    let minutes = date.getMinutes().toString();
+    let hourMinutes = hour + ":" + minutes;
+
+    return hourMinutes
+}
+
+function autoResize() {
+    box.style.cssText = "height:auto; padding:0";
+    box.style.cssText = "height:" + box.scrollHeight + "px";
+}
